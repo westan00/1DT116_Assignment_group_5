85d06b2b75ba7205d554a7deaf7dc1746e18fdd9
anec4889 <anton.eckervald.4889@student.uu.se>
2026-02-26 22:27:29 +0100

    test

--- libpedsim/ped_model.cpp
+++ libpedsim/ped_model.cpp
@@ -529,8 +529,8 @@ void Ped::Model::move(Ped::Model::Region *region) {
       int targetId = find_region(alt.first, alt.second);
       int oldRegionId = find_region(agent->getX(), agent->getY());
 
-      std::unique_lock<std::mutex> lock1(*regionMutexes[oldRegionId]);
-      std::unique_lock<std::mutex> lock2(*regionMutexes[targetId],
+      std::unique_lock<std::mutex> lock1(*regionMutexes[targetId]);
+      std::unique_lock<std::mutex> lock2(*regionMutexes[oldRegionId],
                                          std::defer_lock);
       if (oldRegionId != targetId) {
         lock2.lock();
